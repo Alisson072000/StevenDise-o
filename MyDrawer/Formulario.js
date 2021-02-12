c784137b1e3131ad298ae21b4b9868823878c883
Alisso07200 <marelinchavez1611@gmail.com>
2021-02-12 12:12:31 -0500

    master4

---
+++ MyDrawer/Formulario.js
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import { View, Text, TouchableOpacity, Image, Button, StyleSheet } from 'react-native';
+
+export default function Formulario() {
+    return (
+        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+            <Text style={{ fontSize: 20 }}>Formulario</Text>
+        </View>
+    );
+}
---
+++ MyDrawer/Mapa.js
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import { View, Text, TouchableOpacity, Image, Button, StyleSheet } from 'react-native';
+
+export default function Mapa() {
+    return (
+        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+            <Text style={{ fontSize: 20 }}>Mapa</Text>
+        </View>
+    );
+}
